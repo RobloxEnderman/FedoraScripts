76db79ef325623082aa13d07af50b36566ea6e44
RobloxEnderman <79427951+RobloxEnderman@users.noreply.github.com>
2022-08-21 15:45:08 -0700

    Create brave-stable.sh

---
+++ dnf/install browsers/brave-stable.sh
@@ -0,0 +1,9 @@
+sudo dnf update -y
+sudo dnf install dnf-plugins-core
+sudo dnf update -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf update -y
+sudo dnf install brave-browser
+sleep 2
+echo Brave (Stable) is now installed!
