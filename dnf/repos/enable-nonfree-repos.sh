7b14ec504398beb8a438e1774b09ad94b0096ab9
RobloxEnderman <79427951+RobloxEnderman@users.noreply.github.com>
2022-08-21 15:47:35 -0700

    Create enable-nonfree-repos.sh

---
+++ dnf/repos/enable-nonfree-repos.sh
@@ -0,0 +1,11 @@
+sudo dnf update -y
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  sudo dnf update -y
+  sudo dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf update -y
+sleep 1
+echo The nonfree repos are now enabled!
+echo exiting...
+sleep 1
