2e6f78d58130940f2e9a53f40f09ac90035984c4
RobloxEnderman <79427951+RobloxEnderman@users.noreply.github.com>
2022-08-21 13:52:22 -0400

    Update nvidia-driver-current.sh

--- nvidia/nvidia-driver-current.sh
+++ nvidia/nvidia-driver-current.sh
@@ -3,7 +3,7 @@ sudo dnf install akmod-nvidia
 sudo dnf install xorg-x11-drv-nvidia-cuda
 echo The Nvidia Drivers are now installed.
 echo You have to reboot to apply changes. Then switch to the Xorg option on your DE.
-sleep(3)
+sleep 3
 echo rebooting
-sleep(2)
+sleep 2
 sudo reboot
