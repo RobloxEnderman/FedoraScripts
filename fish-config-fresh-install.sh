5edd77bc8d20ed2c3f26ccca064a0124503c1f98
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-08-25 19:16:53 -0400

    Update fish-config-fresh-install.sh

--- fish-config-fresh-install.sh
+++ fish-config-fresh-install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/fish
 cd ~
 echo  fetching
-cd .config/fish/ 
+cd $HOME/.config/fish/ 
 echo installing
 git clone https://github.com/spaceguybob/Fish-shell-aliases.git -q
 touch config.fish
-echo .config/fish/Fish-shell-aliases/config.fish >> .config/fish/config.fish
-rm -rf .config/fish/Fish-shell-alises/
+echo $HOME/.config/fish/Fish-shell-aliases/config.fish >> config.fish
+rm -rf $HOME/.config/fish/Fish-shell-alises/
 echo ~~~~~~~~~~~~~~~~~~
 cd ~
 echo complete!
