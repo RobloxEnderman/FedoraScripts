a52891b3c4b88f178b8a267de6c179864a94ea9f
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-08-25 19:17:16 -0400

    Update fish-config-reset.sh

--- fish-config-reset.sh
+++ fish-config-reset.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/fish
 cd ~
 echo resetting configs
-rm -rf .config/fish/config.fish
+rm -rf $HOME/.config/fish/config.fish
 git clone https://github.com/spaceguybob/Fish-shell-aliases.git -q
-cd .config/fish/
+cd $HOME/.config/fish/
 touch config.fish
-cat .config/fish/Fish-shell-aliases/default-config.fish >> config.fish
-rm -rf .config/fish/Fish-shell-alises/
-rmdir .config/fish/Fish-shell-alises/
+cat $HOME/.config/fish/Fish-shell-aliases/default-config.fish >> config.fish
+rm -rf $HOME/.config/fish/Fish-shell-alises/
+rmdir $HOME/.config/fish/Fish-shell-alises/
 cd ~
 echo Done!
