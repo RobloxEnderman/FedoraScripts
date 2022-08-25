31dd0838d38889eda0a9365048bb310220618670
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-08-25 19:15:25 -0400

    Update fish-config-reinstall.sh

--- fish-config-reinstall.sh
+++ fish-config-reinstall.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/fish
 cd ~
 echo removing redundant files
-rm -rf .config/fish/backup/
+rm -rf $HOME/.config/fish/backup/
 echo backing up fish configs
-mkdir .config/fish/backup/ 
-mv .config/fish/config.fish .config/fish/backup/
+mkdir $HOME/.config/fish/backup/ 
+mv $HOME/.config/fish/config.fish $HOME/.config/fish/backup/
 sleep 2
 echo ~~~~~~~~~~~~~~~~~~
 echo  fetching
-cd .config/fish/ 
+cd $HOME/.config/fish/ 
 sleep 1
 echo installing
 touch config.fish
 git clone https://github.com/spaceguybob/Fish-shell-aliases.git -q
-cat .config/fish/Fish-shell-aliases/config.fish >> config.fish
-rm -rf .config/fish/Fish-shell-alises/
-rmdir .config/fish/Fish-shell-alises/
+cat $HOME/.config/fish/Fish-shell-aliases/config.fish >> config.fish
+rm -rf $HOME/.config/fish/Fish-shell-alises/
 echo ~~~~~~~~~~~~~~~~~~
 cd ~
 echo complete!
