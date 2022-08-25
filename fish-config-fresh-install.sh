4bf3b018fc9f000afc88deeec8e8b977fb17adc3
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-08-25 18:50:19 -0400

    Add files via upload

---
+++ fish-config-fresh-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/fish
+cd ~
+echo  fetching
+cd .config/fish/ 
+echo installing
+git clone https://github.com/spaceguybob/Fish-shell-aliases.git -q
+touch config.fish
+echo .config/fish/Fish-shell-aliases/config.fish >> .config/fish/config.fish
+rm -rf .config/fish/Fish-shell-alises/
+rmdir .config/fish/Fish-shell-alises/
+echo ~~~~~~~~~~~~~~~~~~
+cd ~
+echo complete!
---
+++ fish-config-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/fish
+cd ~
+echo backing up fish configs
+mkdir .config/fish/backup/ 
+mv .config/fish/config.fish .config/fish/backup/
+sleep 2
+echo ~~~~~~~~~~~~~~~~~~
+echo  fetching
+cd .config/fish/ 
+sleep 1
+echo installing
+touch config.fish
+git clone https://github.com/spaceguybob/Fish-shell-aliases.git -q
+cat .config/fish/Fish-shell-aliases/config.fish >> config.fish
+rm -rf .config/fish/Fish-shell-alises/
+rmdir .config/fish/Fish-shell-alises/
+echo ~~~~~~~~~~~~~~~~~~
+cd ~
+echo complete!
