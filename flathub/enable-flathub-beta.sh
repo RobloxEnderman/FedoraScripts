bc367822688ee0485af45c96bc1266453dc9a811
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-08-25 20:26:47 -0400

    Add files via upload

---
+++ flathub/enable-flathub-beta.sh
@@ -0,0 +1,6 @@
+sudo dnf update -y
+flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+sleep 2
+echo All Done!
+sleep 1
+echo Nothing to do. Exiting...
---
+++ flathub/enable-gnome-nightly.sh
@@ -0,0 +1,6 @@
+sudo dnf update -y
+flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+sleep 2
+echo All Done!
+sleep 1
+echo Nothing to do. Exiting...
