a1557f872561bd856251b5b304402061d90211f7
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-08-30 14:19:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ git clone https://github.com/RobloxEnderman/FedoraScripts -q
 chmod -R 755 $HOME/.scripts/FedoraScripts/
 echo "Done!"
 cat $HOME/.scripts/FedoraScripts/addon.txt >> config.fish
-echo="to install the nvidia scripts run 'nvs' after restarting shell"
+#echo="to install the nvidia scripts run 'nvs' after restarting shell"
+fish
