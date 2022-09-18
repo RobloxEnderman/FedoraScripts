3f8399e26c2ee09e879b15ac7193fb14647bd25c
TheLinuxNerd <79427951+RobloxEnderman@users.noreply.github.com>
2022-09-18 12:02:47 -0700

    Update nvidia-driver-current.sh

--- nvidia/nvidia-driver-current.sh
+++ nvidia/nvidia-driver-current.sh
@@ -4,7 +4,7 @@ sudo dnf install akmod-nvidia
 sudo dnf install xorg-x11-drv-nvidia-cuda
 echo The Nvidia Drivers are now installed.
 sleep 1
-echo You have to reboot to apply changes. Then switch to the Xorg option on your DE.
+echo You have to reboot to apply changes. Then switch to the Xorg option on your Login Manager. Please note that Wayland is not fully supported yet.
 sleep 3
 echo rebooting
 sleep 2
